6732b9ebeda960515cf1293c75a1d8c78156e068
Abijahebe <Abijahebe911@gmail.com>
2023-03-20 05:33:28 -0700

    task

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared *.o -o liball.so
+
+# Compile all .c files into object files
+gcc -c -fPIC *.c
+
+# Create the shared library
+gcc -shared -o liball.so *.o
+
+# Clean up object files
+rm *.o
